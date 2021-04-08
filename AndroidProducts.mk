78a1fd2c21e2696fb1568424d06882fd67ca25f0
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-04-08 12:49:08 +0200

    Add AndroidProducts.mk.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_degaslte.mk
