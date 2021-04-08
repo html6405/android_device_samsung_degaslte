cdff29d07203d8439a7f46557113074fc36b0f44
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-04-08 12:48:16 +0200

    Rename lineage.mk to lineage_degaslte.mk to fix breakfast command.

---
+++ lineage_degaslte.mk
@@ -0,0 +1,6 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/degaslte/full_degaslte.mk)
+
+PRODUCT_NAME := lineage_degaslte
